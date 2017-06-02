f2afb01c333371422045a2980624fb71fac7bef8
Antonio Calapez <antonio@hackages.io>
2017-06-02 16:20:54 +0200

    Adding a basic user profile

--- web-client/src/components/User.js
+++ web-client/src/components/User.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {apiGET} from "../hoc/get";
+import {connect} from "react-redux";
 
-const _Users = props =>
-  <div>
-    {console.log(props)}
-    Users page!
-  </div>;
+class _User extends Component {
 
-export const User = apiGET('/users')(_Users);
+  componentDidMount(){
+    this.props.makeRequest('get', `/users/${this.props.match.params.id}`, null, 'user');
+  }
+
+  render(){
+    const {user, isAdmin} = this.props;
+    if(!user) return null;
+    return (
+      <div className="section">
+        <div className="card">
+          <figure className="card-profile-image">
+            <img
+              className="circle z-depth-2 responsive-img activator"
+              src={user.photoURL}
+              alt="profile image"/>
+          </figure>
+          <div className="card-content">
+            <div className="row">
+              <div className="col s3 offset-s2">
+                <h4 className="card-title grey-text text-darken-4">
+                  {user.displayName}
+                </h4>
+                <p className="medium-small grey-text">
+                  {user.isAdmin ? 'Administrateur' : 'Utilisateur'}
+                </p>
+              </div>
+              <div className="col s2 cent-align">
+                <h4 className="card-title grey-text text-darken-4">
+                  10
+                </h4>
+                <p className="medium-small grey-text">
+                  Chiens
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export const User = apiGET('/users')(_User);
