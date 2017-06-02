0ef8aee9573420785b5783d7490999e0cb67e9e9
Antonio Calapez <antonio@hackages.io>
2017-06-02 23:14:08 +0200

    Updating the /users page

--- web-client/src/components/Users.js
+++ web-client/src/components/Users.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import {apiGET} from "../hoc/get";
+import {connect} from "react-redux";
+import {logout} from "../actions/auth";
 
-const _Users = ({data}) =>
+const _Users = ({data, user_id, makeRequest, logout}) =>
   <div>
-    <ul>
-      {data.map(user => (
-        <li key={user.id}>{user.displayName}</li>
-      ))}
-    </ul>
+    <table className="striped">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>displayName</th>
+        <th>Email</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
+
+      <tbody>
+      {data.map(user=>
+        <tr>
+          <td>{user.id}</td>
+          <td>{user.displayName} {user.id === user_id ? <strong>(vous)</strong> : null}</td>
+          <td>{user.email}</td>
+          <td>
+            <button
+              onClick={() => {
+                makeRequest('delete', `/users/${user.id}`);
+                if(user.id === user_id){
+                  logout();
+                }
+              }}
+              className="btn">
+              Supprimer
+            </button>
+          </td>
+        </tr>
+      )}
+      </tbody>
+    </table>
   </div>;
 
-export const Users = apiGET('/users')(_Users);
+const mapStateToProps = ({auth}) => ({...auth});
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logout())
+});
+
+export const Users = connect(mapStateToProps, mapDispatchToProps)(apiGET('/users')(_Users));
