231fdb58675f31828a6f7c4c273bef04f2f06b64
Antonio Calapez <antonio@hackages.io>
2017-06-02 13:46:16 +0200

    Adding higher order routes for navigation purposes

--- web-client/src/hoc/get.js
+++ web-client/src/hoc/get.js
@@ -1,22 +1,58 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {backend_url} from "../constants/backend";
+import * as axios from "axios";
 
 export const apiGET = (url, initialData = []) => WrappedComponent =>
   class GetHOC extends Component {
-    state = {
-      data: initialData,
-      err: null,
+    static contextTypes = {
+      store: PropTypes.object,
+      router: PropTypes.object,
     };
 
-    componentWillMount(){
-      fetch(`${backend_url}${url}`)
-        .then(data => data.json())
-        .then(data => this.setState({data}))
+    constructor(props){
+      super(props);
+      this.state = {
+        data: initialData,
+        err: null,
+      };
+    }
+
+    refresh = () => {
+      const {token} = this.context.store.getState().auth;
+      axios({
+        method: 'get',
+        url: `${backend_url}${url}`,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token,
+        }
+      }).then(({data}) => this.setState({data}))
+        .catch(err => this.setState(err));
+    };
+
+
+    makeRequest = (method, url, data) => {
+      const {token} = this.context.store.getState().auth;
+      axios({
+        method,
+        url: `${backend_url}${url}`,
+        data,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token,
+        }
+      }).then(this.refresh)
         .catch(err => this.setState({err}));
+    };
+
+    componentDidMount(){
+      // TODO: Cancel the request on componentWillUnmount
+      this.refresh();
     }
 
     render(){
-      const props = {...this.props, ...this.state};
+      const props = {makeRequest: this.makeRequest, ...this.props, ...this.state};
       return <WrappedComponent {...props}/>
     }
   };
