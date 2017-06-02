2e31e76159c5ad3596c8f19d620e641addbea88f
Antonio Calapez <antonio@hackages.io>
2017-06-02 02:15:44 +0200

    Adding a Either utility

--- app/app/Utils.php
+++ app/app/Utils.php
@@ -10,4 +10,8 @@ class Utils {
   public static function getPayload(JwtToken $jwt, Request $request){
     return $jwt->setToken($request->header('Authorization'))->payload();
   }
+
+  public static function Either($left, $right){
+    return $left ? $left : $right;
+  }
 }
