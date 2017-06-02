2d97becd32c22641dd842c8f2849611a3dfa6d6a
Antonio Calapez <antonio@hackages.io>
2017-06-02 17:26:32 +0200

    Using the new navbar component

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -4,32 +4,14 @@ import {Routes} from "./Routes";
 
 import './Routing.css';
 import {connect} from "react-redux";
+import {Nav} from "./components/Nav";
 
 class _App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <nav>
-            <div className="nav-wrapper">
-              <Link to="/" className="brand-logo">Dogemon Go</Link>
-              <ul className="right hide-on-med-and-down">
-                <li><NavLink to="/dogs">Chiens</NavLink></li>
-                <li><NavLink to="/breeds">Races</NavLink></li>
-                <li><NavLink to="/users">Utilisateurs</NavLink></li>
-                {!this.props.isLoggedIn ?
-                  <span>
-                    <li><NavLink to="/login">Se connecter</NavLink></li>
-                    <li>< NavLink to = "/signup" > S'inscrire</NavLink></li>
-                  </span>
-                  :
-                  <span>
-                    <li><NavLink to={`users/${this.props.user_id}`}>Mon profil</NavLink></li>
-                  </span>
-                }
-              </ul>
-            </div>
-          </nav>
+          <Nav />
           <div className="container">
             <Routes />
           </div>
